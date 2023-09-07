41989e17750ceb55941ae725997985a3731d5303
ArchSofia <sofiadiazvaldez@gmail.com>
2023-09-07 18:00:16 -0300

    Agrega primer version del footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,34 @@
 import React from "react";
 
+import { AiFillTwitterCircle } from "react-icons/ai";
+import { AiFillFacebook } from "react-icons/ai";
+import { AiFillLinkedin } from "react-icons/ai";
+import { AiOutlineYoutube } from "react-icons/ai";
+import { AiOutlineInstagram } from "react-icons/ai";
+import { BsSnapchat } from "react-icons/bs";
+import "./Footer.css";
 const Footer = () => {
-	return <div>Footer</div>;
+	return (
+		<>
+			<div className="footercontainer">
+				<div className="firstline">
+					<div className="date">© 2020</div>
+					<div className="socialicons">
+						<AiFillTwitterCircle />
+						<AiFillFacebook />
+						<AiFillLinkedin />
+						<AiOutlineYoutube />
+						<AiOutlineInstagram />
+						<BsSnapchat />
+						<div className="icon"></div>
+					</div>
+				</div>
+				<div className="secondline">
+					<hr />
+				</div>
+			</div>
+		</>
+	);
 };
 
 export default Footer;
