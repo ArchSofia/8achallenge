e067cdcc6b8b338e70331f1018f287e6590672f3
ArchSofia <sofiadiazvaldez@gmail.com>
2023-09-07 14:21:10 -0300

    Crea componente Header completo Desktop version

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,21 @@
 import React from "react";
+import "./Header.css";
 
 const Header = () => {
-	return <div>Header</div>;
+	return (
+		<div className="header">
+			<div className="headerItems">
+				<div className="webinar">WEBINAR</div>
+				<div className="mainTitle">
+					El reto de humanizar el CX financiero en 2021.{" "}
+					<span>La experiencia de un Unicornio de Latam</span>
+				</div>
+				<div className="subtitle">
+					MIÉRCOLES 16 DE DICIEMBRE | 17 HS (HORARIO DE QUITO)
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Header;
