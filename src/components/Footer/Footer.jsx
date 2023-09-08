17e5269a18fbe0517c55acb0e87744a59a5d9340
ArchSofia <sofiadiazvaldez@gmail.com>
2023-09-08 20:13:39 -0300

    corrige iconos de redes sociales en footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,12 +1,13 @@
 import React from "react";
-
-import { AiFillTwitterCircle } from "react-icons/ai";
-import { AiFillFacebook } from "react-icons/ai";
-import { AiFillLinkedin } from "react-icons/ai";
-import { AiOutlineYoutube } from "react-icons/ai";
-import { AiOutlineInstagram } from "react-icons/ai";
-import { BsSnapchat } from "react-icons/bs";
 import "./Footer.css";
+import twitter from "/Users/sofiadiazvaldez/Documents/PROJECTS/Challenge 8a agency/8achallenge/src/assets/5279123_tweet_twitter_twitter logo_icon.svg";
+import fb from "/Users/sofiadiazvaldez/Documents/PROJECTS/Challenge 8a agency/8achallenge/src/assets/5279111_network_fb_social media_facebook_facebook logo_icon.svg";
+import linkdn from "/Users/sofiadiazvaldez/Documents/PROJECTS/Challenge 8a agency/8achallenge/src/assets/5279114_linkedin_network_social network_linkedin logo_icon.svg";
+import yt from "/Users/sofiadiazvaldez/Documents/PROJECTS/Challenge 8a agency/8achallenge/src/assets/5279120_play_video_youtube_youtuble logo_icon.svg";
+import instagram from "/Users/sofiadiazvaldez/Documents/PROJECTS/Challenge 8a agency/8achallenge/src/assets/5279112_camera_instagram_social media_instagram logo_icon.svg";
+import snapch from "/Users/sofiadiazvaldez/Documents/PROJECTS/Challenge 8a agency/8achallenge/src/assets/1964410_logo_media_snapchat_social_icon.svg";
+// TODO: Acortar los paths
+
 const Footer = () => {
 	return (
 		<>
@@ -14,13 +15,24 @@ const Footer = () => {
 				<div className="firstline">
 					<div className="date">© 2020</div>
 					<div className="socialicons">
-						<AiFillTwitterCircle />
-						<AiFillFacebook />
-						<AiFillLinkedin />
-						<AiOutlineYoutube />
-						<AiOutlineInstagram />
-						<BsSnapchat />
-						<div className="icon"></div>
+						<div className="icon">
+							<img src={twitter} alt={twitter} />
+						</div>
+						<div className="icon">
+							<img src={fb} alt={fb} />
+						</div>
+						<div className="icon">
+							<img src={linkdn} alt={linkdn} />
+						</div>
+						<div className="icon">
+							<img src={yt} alt={yt} />
+						</div>
+						<div className="icon">
+							<img src={instagram} alt={instagram} />
+						</div>
+						<div className="icon">
+							<img src={snapch} alt={snapch} />
+						</div>
 					</div>
 				</div>
 				<div className="secondline">
