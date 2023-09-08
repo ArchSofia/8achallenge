ed62f9e9a3afbabace30b363681b6239d595190c
ArchSofia <sofiadiazvaldez@gmail.com>
2023-09-08 12:02:44 -0300

    Ubica los contenedores del componente main

---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "./Main.css";
+const Main = () => {
+	return (
+		<>
+			<div className="maincontainer">
+				<div className="firstcolumn">
+					<div className="sponsors"></div>
+					<div className="content"></div>
+					<div className="sponsors"></div>
+				</div>
+				<div className="secondcolumn"></div>
+			</div>
+		</>
+	);
+};
+
+export default Main;
