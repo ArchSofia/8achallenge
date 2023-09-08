93578e45ba9560cdceed68b381ddfb02421bdd9a
ArchSofia <sofiadiazvaldez@gmail.com>
2023-09-08 12:59:28 -0300

    Agrega primer columna de main completa

--- src/components/Specialist/specialist.jsx
+++ src/components/Specialist/specialist.jsx
@@ -1,7 +1,20 @@
 import React from "react";
+import "./Specialist.css";
 
-const specialist = () => {
-	return <div>specialist</div>;
+const Specialist = ({ nombre, rol, fotoURL }) => {
+	return (
+		<>
+			<div className="mini-card">
+				<div className="circle-avatar">
+					{/* <img src={fotoURL} alt={nombre} /> */}
+				</div>
+				<div className="info">
+					<h3>{nombre}</h3>
+					<p>{rol}</p>
+				</div>
+			</div>
+		</>
+	);
 };
 
-export default specialist;
+export default Specialist;
