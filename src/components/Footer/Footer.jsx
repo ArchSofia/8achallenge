4a35c0cfd4b7cfadcb619dcf256cc977484e9771
ArchSofia <sofiadiazvaldez@gmail.com>
2023-09-08 21:24:43 -0300

    corrige paths de imagenes  a paths relativos

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,12 +1,11 @@
 import React from "react";
 import "./Footer.css";
-import twitter from "/Users/sofiadiazvaldez/Documents/PROJECTS/Challenge 8a agency/8achallenge/src/assets/5279123_tweet_twitter_twitter logo_icon.svg";
-import fb from "/Users/sofiadiazvaldez/Documents/PROJECTS/Challenge 8a agency/8achallenge/src/assets/5279111_network_fb_social media_facebook_facebook logo_icon.svg";
-import linkdn from "/Users/sofiadiazvaldez/Documents/PROJECTS/Challenge 8a agency/8achallenge/src/assets/5279114_linkedin_network_social network_linkedin logo_icon.svg";
-import yt from "/Users/sofiadiazvaldez/Documents/PROJECTS/Challenge 8a agency/8achallenge/src/assets/5279120_play_video_youtube_youtuble logo_icon.svg";
-import instagram from "/Users/sofiadiazvaldez/Documents/PROJECTS/Challenge 8a agency/8achallenge/src/assets/5279112_camera_instagram_social media_instagram logo_icon.svg";
-import snapch from "/Users/sofiadiazvaldez/Documents/PROJECTS/Challenge 8a agency/8achallenge/src/assets/1964410_logo_media_snapchat_social_icon.svg";
-// TODO: Acortar los paths
+import twitter from "../../assets/5279123_tweet_twitter_twitter logo_icon.svg";
+import fb from "../../assets/5279111_network_fb_social media_facebook_facebook logo_icon.svg";
+import linkdn from "../../assets/5279114_linkedin_network_social network_linkedin logo_icon.svg";
+import yt from "../../assets/5279120_play_video_youtube_youtuble logo_icon.svg";
+import instagram from "../../assets/5279112_camera_instagram_social media_instagram logo_icon.svg";
+import snapch from "../../assets/1964410_logo_media_snapchat_social_icon.svg";
 
 const Footer = () => {
 	return (
